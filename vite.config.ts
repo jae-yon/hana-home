f35792617709deceb605349935613ee650e8bcdf
JaeYoung <kimjaeyoung650@gmail.com>
2026-02-25 19:49:54 +0900

    [CHORE] 이미지 최적화 플러그인 추가 및 빌드 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        // 품질 (0~100)
+        quality: 80,
+      },
+      jpeg: {
+        quality: 75,
+      },
+      jpg: {
+        quality: 75,
+      },
+      webp: {
+        lossless: true,
+      },
+      avif: {
+        lossless: true,
+      },
+    }),
+  ],
   base: '/',
   resolve: {
     alias: {
